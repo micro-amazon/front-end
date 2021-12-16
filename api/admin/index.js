3c388682b552254051116d5daf0253c0bb1fc743
zwan <2zwan@naver.com>
2021-12-16 20:26:58 +0900

    fix: add content-type

--- api/admin/index.js
+++ api/admin/index.js
@@ -4,7 +4,9 @@
     var async = require("async"), express = require("express"), request = require("request"), endpoints = require("../endpoints"), helpers = require("../../helpers"), app = express(), cookie_name = "logged_in"
 
     app.post("/admin/login", function (req, res, next) {
+        var headers = {'content-type' : 'application/x-www-form-urlencoded'};
         var options = {
+            headers,
             uri: `${endpoints.adminUrl}/login`,
             method: 'POST',
             json: true,
@@ -17,6 +19,7 @@
                 console.log(error);
                 return;
             }
+            console.log(response)
             if (response.statusCode == 200) {
                 if (body.error) {
                     return;
