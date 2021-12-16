1a0db3660677ffd373b6baecadad6412c2a77c41
zwan <2zwan@naver.com>
2021-12-16 19:40:14 +0900

    Revert "fix: fix uri test"
    This reverts commit 9a86d46019f10881bd8bc319a43133559bc4a402.

--- api/admin/index.js
+++ api/admin/index.js
@@ -5,7 +5,7 @@
 
     app.post("/admin/login", function (req, res, next) {
         var options = {
-            uri: `${endpoints.adminUrl}/admin/login`,
+            uri: `${endpoints.adminUrl}/login`,
             method: 'POST',
             json: true,
             body: req.body
@@ -31,6 +31,7 @@
                 return;
             }
         });
+        return;
     });
 
     module.exports = app;
